3962f542ec31a4e801aaf404767573b2bf7b199e
Paul <paulpeng@xtremeprog.com>
2016-09-23 17:06:40 +0800

    add socket

---
+++ socket.php
@@ -0,0 +1,30 @@
+<?php
+$gizwits_noti_host = "noti.gizwits.com";
+$gizwits_noti_port = 2015;
+$enterpriseId = "ccd3b1f498be4c21a30a938fe400ce64";
+$enterpriseSecret = "c54dad24bffb4881adb2b12c76d7a2e0";
+
+function sendLoginMsg($enterpriseId, $enterpriseSecret) {
+    $data = array(
+        "data" => 
+            array("enterprise_id" => $enterpriseId,
+                  "enterprise_secret" =>  $enterpriseSecret,
+                  "prefetch_count" => 50),
+        "cmd"=> "enterprise_login_req"
+    );
+    $msg = strval(json_encode($data)."\n");
+    return $msg;
+}
+
+$msg = sendLoginMsg($enterpriseId, $enterpriseSecret);
+
+$fp = stream_socket_client("ssl://noti.gizwits.com:2015", $errno, $errstr, 30);  
+fwrite($fp, $msg);  
+$response = "";
+while (!feof($fp) ) {  
+    $r = fgets($fp, 1024);  
+    $response .= $r;  
+    //处理这一行  
+}  
+print_r($response);
+?>
